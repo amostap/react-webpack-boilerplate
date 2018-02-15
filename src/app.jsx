f6fc95b611e88e6cf58a6dfeaf9e3d6ac538aa23
Alexandr Ostapenko <alexandr.amostap@gmail.com>
2018-02-16 00:55:21 +0200

    removed route

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { LocaleProvider } from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
 import configureStore from 'store/configureStore';
@@ -14,7 +14,7 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <LocaleProvider locale={enUS}>
-        <Route component={App} />
+        <App />
       </LocaleProvider>
     </BrowserRouter>
   </Provider>,
